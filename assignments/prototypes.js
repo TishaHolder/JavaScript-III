25fe717fb821f04904307314cc2cdb679176337e
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-07-24 15:56:10 -0400

    completed prototye.js and this.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,35 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject (gameAttributes) {
+  this.createdAt = gameAttributes.createdAt;
+  this.name = gameAttributes.name;
+  this.dimensions = gameAttributes.dimensions;  
+}
+
+GameObject.prototype.destroy = function() {
+  console.log (`${this.name} was removed from the game.`);
+}
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats (characterAttributes) {
+  GameObject.call(this, characterAttributes);//sets CharacterStat's prototype to GameObject's prototype
+  this.healthPoints = characterAttributes.healthPoints;  
+}
+
+//in order to ‘inherit’ the prototype methods from the GameObject's prototype 
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+//prototype method
+CharacterStats.prototype.takeDamage = function () {
+  console.log(`${this.name} took damage.`);
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,6 +55,26 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+function Humanoid (humanoidAttributes) {
+  GameObject.call(this, humanoidAttributes);//sets Humanoid's prototype to GameObject's prototype
+  CharacterStats.call(this, humanoidAttributes);//sets Humanoid's prototype to CharacterStat's prototype
+  
+  this.team = humanoidAttributes.team;
+  this.weapons = humanoidAttributes.weapons;
+  this.language = humanoidAttributes.language;
+}
+
+//in order to ‘inherit’ the prototype methods from the GameObject & CharacterStat prototypes
+Humanoid.prototype = Object.create(GameObject.prototype);
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+//prototype method
+Humanoid.prototype.greet = function() {
+  console.log(`${this.name} offers a greeting in ${this.language}`);
+}
+
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +84,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +145,46 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  //==============Villain Constructor Function
+  function Villain (villainAttributes) {
+    Humanoid.call(this, villainAttributes);//sets Villains's prototype to Humanoid's prototype
+  }
+
+  //in order to ‘inherit’ the prototype methods from the Humanoid prototypes
+  Villain.prototype = Object.create(Humanoid.prototype);
+
+  /*prototype method - remove health points from objects which could result in destruction if health gets to 0 or drops below 0;*/
+  Villain.prototype.removeHealthPoints = function() {
+    
+    /*return this.healthpoints--;
+
+    if(this.healthPoints) {
+      return true; 
+    }   
+    else {
+      return false;
+    }*/  
+   
+  }
+
+  //==============Hero Constructor Function
+  function Hero (heroAttributes) {
+    Humanoid.call(this, heroAttributes);//sets Hero's prototype to Humanoid's prototype
+    
+  }
+
+  //in order to ‘inherit’ the prototype methods from the Humanoid prototypes
+  Hero.prototype = Object.create(Humanoid.prototype);
+
+  //prototype method - add health points to non-villain objects tif health gets to 0 or drops below 0;
+  Hero.prototype.addHealthPoints = function() {
+    
+  }
+
